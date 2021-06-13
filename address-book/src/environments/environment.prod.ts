f613e1f7a65111489cbd99d917db991b8d39c46a
Dimas-Rizki-Novyadi <dimas1900016071@webmail.uad.ac.id>
2021-06-13 08:20:15 +0700

    tugas akhir(update)

--- address-book/src/environments/environment.prod.ts
+++ address-book/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAiPUDjhVM4gO9V6OFFLfJzM0vk_7PfpXs",
+    authDomain: "address-book-ee657.firebaseapp.com",
+    projectId: "address-book-ee657",
+    storageBucket: "address-book-ee657.appspot.com",
+    messagingSenderId: "267054947041",
+    appId: "1:267054947041:web:1f0bd4d2e6216b9cd7f5a0"
+  }
 };
